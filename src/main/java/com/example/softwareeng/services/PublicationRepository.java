6f5882ca30a9330e7981ff2f941c2600adab0b64
Bikram Khanal <bikram.khanal@aiesec.net>
2021-11-26 13:05:40 -0600

    blocked publication

--- src/main/java/com/example/softwareeng/services/PublicationRepository.java
+++ src/main/java/com/example/softwareeng/services/PublicationRepository.java
@@ -154,11 +154,7 @@ public class PublicationRepository {
     }
 
 
-    public List<Publication> getBlockedPublications(Long personid) {
-        Query query = em.createQuery("SELECT c.blockedPublications FROM Person c where c.id = :personid");
-        query.setParameter("personid", personid).getResultList();
-        return query.getResultList();
-    }
+
 
 
     public List<Publication> getNotBlockedPublications(Long personid) {
